e01a44e6d595471bc3a77ef9aae1e1f22dc641e4
Arthur <Arthurchen1130@gmail.com>
2026-07-08 00:04:09 -0400

    Added the ability to draw the tiles

--- vga_controller.sv
+++ vga_controller.sv
@@ -20,12 +20,12 @@
     logic [2:0] rgb;
     logic [9:0] h_count;
     logic [8:0] v_count;
-    logic [2:0] tile_data;
+    logic [1:0] tile_data;
     
     // test values
-    assign h_count = 10'd110;
-    assign v_count = 9'd100;
-    assign tile_data = 3'd0;
+    assign h_count = 10'd3;
+    assign v_count = 9'd3;
+    assign tile_data = 2'b11;
     
     draw_tile dt_test (
       .rgb(rgb),
@@ -46,29 +46,64 @@
     output logic [2:0]rgb,      //pixel color
     input logic [9:0]h_count,   //Horizontal Display Counter (max value is 640px)
     input logic [8:0]v_count,    //Vertical Display Counter (maxvalue is 480px)
-    input logic [2:0] tile_data //Tile Data (blank, wall, pellet, power pellet)
+    input logic [1:0] tile_data //TEMPORARILY IT CODE SHOULD KNOW WHAT TILE IT IS FROM h_count and v_count... Tile Data (blank, wall, pellet, power pellet)
   );
-  
+
+  //variables
   logic black_left, black_bottom;
+  logic [4:0] tile_y;     // 0-27 Y tiles
+  logic [5:0] tile_x;     // 0-35 X tiles
+  logic [2:0] pixel_x, pixel_y; //0-7 pixels in tiles
   
   //determine whether pixel is in the map
   assign black_left   = ((h_count >= 416) && (h_count < 640) && (v_count >= 0)   && (v_count < 480));
   assign black_bottom = ((h_count >= 0)   && (h_count < 640) && (v_count >= 192) && (v_count < 480));
   
-  //draw black if not on map
+  //determine tile location and pixel position in tile
+  assign tile_y = v_count[7:3]; //quotient
+  assign tile_x = h_count[8:3];
+  assign pixel_y = v_count[2:0]; //remainder
+  assign pixel_x = h_count[2:0];
+  
+  //pixel generator for tile
   always_comb begin
-    if (black_left) //black border when h_count is higher than 416px
+    //draw black if not on map
+    if (black_left)
       rgb = 3'b000;
-    else if (black_bottom) //black border when v_count is higher than 192px
+    else if (black_bottom)
       rgb = 3'b000;
+  
+    //draw blue wall if applicable
+    else if (tile_data == 2'b01) begin // wall
+      rgb = 3'b001;
+    end
+    
+    //draw 2 by 2 white square in center if applicable
+    else if (tile_data == 2'b10) begin // pellet
+      if ((pixel_x == 3 || pixel_x == 4) &&
+          (pixel_y == 3 || pixel_y == 4))
+        rgb = 3'b111;
+      else
+        rgb = 3'b000;
+    end
+  
+    //draw 6 by 6 white square with black corners if applicable 
+    else if (tile_data == 2'b11) begin // power pellet
+      if (pixel_x > 0 && pixel_x < 7 &&
+          pixel_y > 0 && pixel_y < 7) begin
+  
+        // black corners
+        if ((pixel_x == 1 || pixel_x == 6) &&
+            (pixel_y == 1 || pixel_y == 6))
+          rgb = 3'b000;
+        else
+          rgb = 3'b111;
+  
+      end else
+        rgb = 3'b000;
+    end
+  
     else
-      rgb = 3'b111;
+      rgb = 3'b000; // blank
   end
-  //get pixel position using division, quotient is tile position and remainder is pixel position in tile
-  
-  
-
-  
-
-  
   endmodule
